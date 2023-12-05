8b03c7f139ac135b957209021d67e9c3dab6575b
Antho <anthony.jarrety@gmail.com>
2023-12-05 20:18:56 +0100

    update

--- run.py
+++ run.py
@@ -396,6 +396,7 @@ def unknown_command(update: Update, context: CallbackContext) -> None:
     """
     print(update.effective_message.chat.username)
     if(not(update.effective_message.chat.username == TELEGRAM_USER)):
+        update.effective_message.reply_text(update.effective_message.chat.username)
         update.effective_message.reply_text("You are not authorized to use this bot! 🙅🏽‍♂️")
         return
 
@@ -440,6 +441,11 @@ def help(update: Update, context: CallbackContext) -> None:
     update.effective_message.reply_text(commands)
     update.effective_message.reply_text(trade_example + market_execution_example + limit_example + note)
 
+update.effective_message.reply_text("Anthony?")
+    print(update.effective_message.chat.username)
+
+    print(update.effective_message)
+
     return
 
 def cancel(update: Update, context: CallbackContext) -> int:
@@ -476,10 +482,12 @@ def Trade_Command(update: Update, context: CallbackContext) -> int:
         update: update from Telegram
         context: CallbackContext object that stores commonly used objects in handler callbacks
     """
+    update.effective_message.reply_text(update.effective_message.chat.username)
     print('anthony')
     print(update.effective_message.chat.username)
     print('anthony2')
     if(not(update.effective_message.chat.username == TELEGRAM_USER)):
+        update.effective_message.reply_text(update.effective_message.chat.username)
         update.effective_message.reply_text("You are not authorized to use this bot! 🙅🏽‍♂️")
         return ConversationHandler.END
     
@@ -500,6 +508,7 @@ def Calculation_Command(update: Update, context: CallbackContext) -> int:
     """
     print(update.effective_message.chat.username)
     if(not(update.effective_message.chat.username == TELEGRAM_USER)):
+        update.effective_message.reply_text(update.effective_message.chat.username)
         update.effective_message.reply_text("You are not authorized to use this bot! 🙅🏽‍♂️")
         return ConversationHandler.END
 
